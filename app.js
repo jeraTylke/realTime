dddf35dae25f96f55f4e73e96c0ce40df3a88c29
Jeralise Tylke <jtylke@mcad.edu>
2018-10-15 17:38:49 -0500

    built the game cycle and set the amount of time in the cycles. the game resets after a certain amount of time. Overall built the core infrastructure to the drawing game.

---
+++ app.js
@@ -0,0 +1,112 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    socketIo = require('socket.io');
+
+// start webserver on port 8080
+var server =  http.createServer(app);
+var io = socketIo.listen(server);
+server.listen(3000);
+// add directory with our static files
+app.use(express.static(__dirname + '/public'));
+console.log("The server is running: 3000 ");
+
+// array of all lines drawn
+var line_history = [];
+let textResponses = []
+
+// event-handler for new incoming connections
+io.on('connection', function (socket) {
+
+   // first send the history to the new client
+   for (var i in line_history) {
+      socket.emit('draw_line', { line: line_history[i] } );
+   }
+
+   // add handler for message type "draw_line".
+   socket.on('draw_line', function (data) {
+      // add received line to history
+      line_history.push(data.line);
+      // send line to all clients
+      io.emit('draw_line', { line: data.line });
+   });
+
+   socket.on('clearLineHistory',function(){
+     console.log('clear message recd');
+     //clear the line history, by refreshing the array with noting!
+      line_history = []
+      io.emit('clearClient', '')
+      console.log(line_history);
+
+
+   })
+   //add the text entries to the drawing name array.
+   socket.on('uploadText',function(drawingname){
+     textResponses.push(drawingname);
+   })
+
+   socket.on('selectedDrawingName',function(name){
+     io.emit('finalDrawingName', name)
+   })
+});
+
+
+
+
+
+
+
+gameTiming(); //initialize the gametiming.
+
+function gameTiming(){
+  console.log('choose drawing player and spectators')
+
+  io.clients((error,clients)=>{
+    console.log(clients);
+    let randomSocketID = clients[Math.floor(Math.random()*clients.length)]
+    console.log(randomSocketID);
+    io.to(randomSocketID).emit('drawingSelect', true)
+  })
+
+
+  setTimeout(function(){
+      console.log('drawing done, enable text feedback')
+      io.emit('feedbackText', 'hi')
+
+    setTimeout(function(){
+        console.log('text feedback done, enable drawer selection')
+          io.emit('drawerTextSelectionDisplay', textResponses)
+          console.log(textResponses);
+      setTimeout(function(){
+              console.log('selection done, show results')
+              //we dont really need to do anything here,but the space might be nice for a buffer or another kind of interaction...
+        setTimeout(function(){
+            console.log('reset game')
+            //reset everyones status
+            io.emit('drawingSelect', false) //reset everyone to not drawing
+            textResponses = [] //clear the text entries
+            io.emit('clearClient', '') //clear the drawing
+            gameTiming();
+        },20000)
+      },20000)
+    }, 20000)
+  }, 10000)
+}
+
+// setTimeout(function(){
+//
+// },30000)
+
+// let totalTime = 60;
+// let timeFrame = 60;
+//
+// let gameTimer = setInterval(function(){
+//   totalTime -- //tick down the timer.
+//
+//   if(totalTime < timeFrame ){
+//     //timer is up.
+//     //game is over,
+//
+//   }
+//
+// }, 1000)
--- public/frontend.js
+++ public/frontend.js
@@ -1,38 +1,130 @@
-let socket = io.connect();
+// let socket = io.connect();
+
+let isDrawing = false;
 
 
-socket.on('connect', function(){
-  console.log('we connected to the server as:');
-  console.log(socket.id);
 
-})
-  $('body'). click(function(event){
-    console.log('we clicked');
-    console.log(event.clientX, event.clientY);
+document.addEventListener("DOMContentLoaded", function() {
+   var mouse = {
+      click: false,
+      move: false,
+      pos: {x:0, y:0},
+      pos_prev: false
+   };
+   // get canvas element and create context
+   var canvas  = document.getElementById('drawing');
+   var context = canvas.getContext('2d');
+   var width   = window.innerWidth;
+   var height  = window.innerHeight;
+   var socket  = io.connect();
 
-    $('<div>💯</div>').css({
-      'position': 'absolute',
-      'top': event.clientY,
-      'left': event.clientX
-    }).appendTo('body')
+   socket.on('connect', function(){
+     console.log("myid: " + socket.id);
+   })
 
-    //package up the mouse data to send to the server
-    let mouseData = {
-      'mouseX' : event.clientX,
-      'mouseY' : event.clientY
-    }
+   socket.on('drawingSelect',function(status){
+     console.log('drawingSelect happened ' + status);
+     isDrawing = status; //we can draw now, we're the drawer
+      if(isDrawing == true){
+        $('.drawingSelectStatus').show();
+      }
 
-    //send the data up to the server to deal with it!
-    socket.emit('addEmoji', mouseData);
+   })
 
-  })
-  //listen for the send mass emoji message
-  socket.on('sendMassEmoji', function(mouseData){
-    //do something with the incoming mouseData from everyone else
-    $('<div>@</div>').css({
-      'position': 'absolute',
-      'top': mouseData.mouseY,
-      'left': mouseData.mouseX
-    }).appendTo('body')
+   $('.textSubmitButton').click(function(){
+     let drawingname = $('.textEntry').val(); //give the interior textEntry
+     socket.emit('uploadText', drawingname)
+     $('.nameDrawing').fadeOut();
 
-  })
+   })
+
+   socket.on('drawerTextSelectionDisplay',function(textResponses){
+
+     if(isDrawing == true){
+       //show the responses
+       isDrawing = false //turns off drawing capability. (this is maybe in the wrong place)
+       $('.responses').append('Choose the best drawing name:')
+       for(let response in textResponses){
+         $('.responses ul').append('<li>' + textResponses[response] + '</li>')
+       }
+       //click on a response
+       $('.responses ul li').click(function(e){
+         console.log( this.innerText );
+         socket.emit('selectedDrawingName', this.innerText )
+         $('.responses').fadeOut();
+       })
+
+     }else{
+       $('.nameDrawing').fadeOut();
+     }
+   })
+
+   socket.on('feedbackText',function(){
+     //check to see if were not the drawer
+     if(isDrawing == false){
+       // show text entry
+       $('.nameDrawing').show();
+
+     }
+   })
+
+   socket.on('finalDrawingName', function(name){
+     $('.drawingName').text(name)
+     $('.drawingName').fadeIn();
+   })
+
+
+   // $('.clear').click(function(){
+   //   console.log('yo?');
+   //   socket.emit('clearLineHistory', '')
+   //
+   // })
+
+   //all of the stuff we need to do on the client side to reset tha game
+   socket.on('clearClient',function(){
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      $('.drawingName').fadeOut(); //stop showing the final drawing name.
+      $('.responses').html('<ul></ul>') //clear the responses
+      $('.drawingSelectStatus').hide();
+
+   })
+
+   // set canvas to full browser width/height
+   canvas.width = width;
+   canvas.height = height;
+
+   // register mouse event handlers
+   canvas.onmousedown = function(e){ mouse.click = true; };
+   canvas.onmouseup = function(e){ mouse.click = false; };
+
+   canvas.onmousemove = function(e) {
+      // normalize mouse position to range 0.0 - 1.0
+      mouse.pos.x = e.clientX / width;
+      mouse.pos.y = e.clientY / height;
+      mouse.move = true;
+   };
+
+   // draw line received from server
+	socket.on('draw_line', function (data) {
+      var line = data.line;
+      context.beginPath();
+      context.moveTo(line[0].x * width, line[0].y * height);
+      context.lineTo(line[1].x * width, line[1].y * height);
+      context.stroke();
+   });
+
+   // main loop, running every 25ms
+   function mainLoop() {
+      // check if the user is drawing
+      if (mouse.click && mouse.move && mouse.pos_prev) {
+         // send line to to the server
+        if(isDrawing == true){
+         socket.emit('draw_line', { line: [           mouse.pos, mouse.pos_prev ] });
+          mouse.move = false;
+          }
+        }
+      mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+      setTimeout(mainLoop, 25);
+   }
+   mainLoop();
+});
